6ff8578b2d15ab8c8e83abe4605b90d7063bef96
sproshchaev <sproshchaev@gmail.com>
2022-10-25 16:12:51 +0500

    configuration-server: added package config

---
+++ configuration-server/src/main/java/com/prosoft/configurationserver/config/ConfigApp.java
@@ -0,0 +1,11 @@
+package com.prosoft.configurationserver.config;
+
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
+
+
+@EnableConfigServer
+@Configuration
+public class ConfigApp {
+
+}
