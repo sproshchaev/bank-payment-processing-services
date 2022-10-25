cd585945842bed11900f31118094ed4d66a8c49b
sproshchaev <sproshchaev@gmail.com>
2022-10-25 15:51:14 +0500

    Added util.shell

---
+++ sales-point/src/main/java/com/prosoft/salespoint/util/shell/AppEventsCommands.java
@@ -0,0 +1,23 @@
+package com.prosoft.salespoint.util.shell;
+
+import org.h2.tools.Console;
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+
+
+import java.sql.SQLException;
+
+
+@ShellComponent
+public class AppEventsCommands {
+
+    @ShellMethod(value = "Start console H2", key = {"c", "console"})
+    public void startConsoleH2() {
+        try {
+            Console.main();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
