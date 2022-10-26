899745a2c3a9cf55206b5ea175ac6ca23a26cbc8
sproshchaev <sproshchaev@gmail.com>
2022-10-26 13:42:36 +0500

    sales-point: updated paymentAuthorizationService (shell)

--- sales-point/src/main/java/com/prosoft/salespoint/util/shell/AppEventsCommands.java
+++ sales-point/src/main/java/com/prosoft/salespoint/util/shell/AppEventsCommands.java
@@ -1,15 +1,26 @@
 package com.prosoft.salespoint.util.shell;
 
+import com.prosoft.salespoint.model.dto.PaymentValueObject;
+import com.prosoft.salespoint.service.PaymentAuthorizationService;
 import org.h2.tools.Console;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
-
+import org.springframework.shell.standard.ShellOption;
 
 import java.sql.SQLException;
 
 
 @ShellComponent
 public class AppEventsCommands {
+
+    // todo внедрить PaymentAuthorizationServiceImpl через интерфейс
+    private final PaymentAuthorizationService paymentAuthorizationService;
+
+    @Autowired
+    public AppEventsCommands(PaymentAuthorizationService paymentAuthorizationService) {
+        this.paymentAuthorizationService = paymentAuthorizationService;
+    }
 
     @ShellMethod(value = "Start console H2", key = {"c", "console"})
     public void startConsoleH2() {
@@ -19,5 +30,32 @@ public class AppEventsCommands {
             throw new RuntimeException(e);
         }
     }
+
+    /**
+     * make authorization - провести авторизацию новой покупки по банковской карте:
+     * - номер карты '4123450101654724'
+     * - срок действия '2025-12-31'
+     * - сумма операции '500.55'
+     * - валюта операции 'RUB'
+     *
+     * @return: "операция одобрена, код авторизации: 123456"
+     */
+    @ShellMethod(value = "Make authorization", key = {"ma", "makeauthorization"})
+    public String paymentAuthorizationService(@ShellOption(defaultValue = "000000001") String tid,
+                                                          @ShellOption(defaultValue = "2022-10-26") String date,
+                                                          @ShellOption(defaultValue = "4123450101654724") String card,
+                                                          @ShellOption(defaultValue = "1225") String expdate,
+                                                          @ShellOption(defaultValue = "500.55") String sum,
+                                                          @ShellOption(defaultValue = "RUB") String curr) {
+        return paymentAuthorizationService.makeAuthorization(new PaymentValueObject(tid, date, card, expdate, sum,
+                curr)).toString();
+    }
+
+    /**
+     *  list of operations - получить список всех операций (за дату - по умолчанию за сегодня)
+     *  @return: "1) 4123***4724 500.55 RUB к.а.: 123456
+     *            2) 3123***0000 155.95 RUB к.а.: 123457
+     *            Итого: 656.5 RUB
+     */
 
 }
