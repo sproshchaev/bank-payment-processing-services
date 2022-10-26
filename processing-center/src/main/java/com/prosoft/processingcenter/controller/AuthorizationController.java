05e4edc26a97acfc9c40e3995f038711392cb874
sproshchaev <sproshchaev@gmail.com>
2022-10-26 16:18:11 +0500

    processing-center: updated AuthorizationController

--- processing-center/src/main/java/com/prosoft/processingcenter/controller/AuthorizationController.java
+++ processing-center/src/main/java/com/prosoft/processingcenter/controller/AuthorizationController.java
@@ -1,6 +1,8 @@
 package com.prosoft.processingcenter.controller;
 
 import com.prosoft.processingcenter.model.dto.Payment;
+import com.prosoft.processingcenter.service.AuthorizationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,17 +12,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/authorization")
 public class AuthorizationController {
 
-    // private final static String AUTHORIZATION_URL =
+    private final AuthorizationService authorizationService;
 
-    // внедряем сервис авторизации через конструктор
-    public AuthorizationController() {
+    @Autowired
+    public AuthorizationController(AuthorizationService authorizationService) {
+        this.authorizationService = authorizationService;
     }
 
     @GetMapping(value = "/tid/{tid}/date/{date}/card/{card}/expdate/{expdate}/sum/{sum}/curr/{curr}")
     public Payment paymentAuthorization(@PathVariable String date, @PathVariable String tid, @PathVariable String card,
                                         @PathVariable String expdate, @PathVariable String sum,
                                         @PathVariable String curr) {
-        return new Payment(1, tid, date, card, expdate, sum, curr, "00", "123456");
-    }  // todo id поправить - сейчас в константе
+        return authorizationService.paymentAuthorization(new Payment(tid, date, card, expdate, sum, curr));
+    }
 
 }
