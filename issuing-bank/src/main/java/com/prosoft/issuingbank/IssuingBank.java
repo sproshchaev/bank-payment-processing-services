a91c7d8ef126759c2fe628449c819452e94ffd28
sproshchaev <sproshchaev@gmail.com>
2022-10-26 16:24:33 +0500

    Updated Main.class

--- issuing-bank/src/main/java/com/prosoft/issuingbank/IssuingBank.java
+++ issuing-bank/src/main/java/com/prosoft/issuingbank/IssuingBank.java
@@ -3,12 +3,6 @@ package com.prosoft.issuingbank;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Приложение "Банк эмитент"
- *
- * @author Sergey Proshchaev
- * @version 1.0
- */
 @SpringBootApplication
 public class IssuingBank {
 
