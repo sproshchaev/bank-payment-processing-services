5b330df3e04a8c8626e8ed9002251bddfebbdca2
sproshchaev <sproshchaev@gmail.com>
2022-10-28 12:11:53 +0500

    Added send new card

--- issuing-bank/src/main/java/com/prosoft/issuingbank/util/shell/AppEventsCommands.java
+++ issuing-bank/src/main/java/com/prosoft/issuingbank/util/shell/AppEventsCommands.java
@@ -1,6 +1,9 @@
 package com.prosoft.issuingbank.util.shell;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.prosoft.issuingbank.service.ProcessingCenterMessageService;
 import org.h2.tools.Console;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 
@@ -10,6 +13,13 @@ import java.sql.SQLException;
 @ShellComponent
 public class AppEventsCommands {
 
+    private final ProcessingCenterMessageService processingCenterMessageService;
+
+    @Autowired
+    public AppEventsCommands(ProcessingCenterMessageService processingCenterMessageService) {
+        this.processingCenterMessageService = processingCenterMessageService;
+    }
+
     @ShellMethod(value = "Start console H2", key = {"c", "console"})
     public void startConsoleH2() {
         try {
@@ -18,5 +28,11 @@ public class AppEventsCommands {
             throw new RuntimeException(e);
         }
     }
+
+    @ShellMethod(value = "Send new cards to processing-center", key = {"sc", "sendcard"})
+    public void sendNewCard() throws JsonProcessingException {
+        processingCenterMessageService.sendMessage();
+    }
+
 
 }
