2ea28d5a1bac1c7eb0ca64dfd4cbca562aa87cb6
sproshchaev <sproshchaev@gmail.com>
2022-10-28 17:01:32 +0500

    Added packeges to currency-converter

---
+++ currency-converter/src/main/java/com/prosoft/currencyconverter/config/ConfigApp.java
@@ -0,0 +1,9 @@
+package com.prosoft.currencyconverter.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@EnableJpaRepositories
+@Configuration
+public class ConfigApp {
+}
---
+++ currency-converter/src/main/java/com/prosoft/currencyconverter/util/shell/AppEventsCommands.java
@@ -0,0 +1,37 @@
+package com.prosoft.currencyconverter.util.shell;
+
+import org.h2.tools.Console;
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.standard.ShellOption;
+
+import java.sql.SQLException;
+
+
+@ShellComponent
+public class AppEventsCommands {
+
+    @ShellMethod(value = "Start console H2", key = {"c", "console"})
+    public void startConsoleH2() {
+        try {
+            Console.main();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+/*
+    @ShellMethod(value = "Make authorization", key = {"ma", "makeauthorization"})
+    public String paymentAuthorizationService(@ShellOption(defaultValue = "000000001") String tid,
+                                              @ShellOption(defaultValue = "2022-10-26") String date,
+                                              @ShellOption(defaultValue = "4123450101654724") String card,
+                                              @ShellOption(defaultValue = "1225") String expdate,
+                                              @ShellOption(defaultValue = "500.55") String sum,
+                                              @ShellOption(defaultValue = "RUB") String curr) {
+        return authorizationService.paymentAuthorization(new Payment(tid, date, card, expdate, sum,
+                curr)).toString();
+    }
+*/
+
+
+}
